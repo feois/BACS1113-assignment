29ae94775616275ad3f5970b488d2680015f5a81
wilson <wilsona4135@gmail.com>
2025-04-03 11:03:33 +0800

    interest + print float fixing

--- assignment.asm
+++ assignment.asm
@@ -6,6 +6,7 @@ buffer db BUFFER_LENGTH + 1 dup (?)
 
 ; constant
 float_ten real4 10.0
+float_hundred real4 100.0
 ; generic variable used to store float (in IEEE single-precision format) temporarily
 float_register real4 ?
 
@@ -36,19 +37,29 @@ options dd offset option_loan, offset option_interest, offset option_debt, offse
 option_dialog db "Please select a valid option (1-", '0' + lengthof options, "): ", 0
 selected_option dd ?
 
-loan_dialog db "Please enter the following values", 0
+values_dialog db "Please enter the following values", 0
+
 loan_p_dialog db "Principal: RM ", 0
 loan_r_dialog db "Monthly interest rate (in %): ", 0
 loan_n_dialog db "Number of payments: ", 0
-loan_principal dd 0
-loan_rate real4 0.0
-loan_payment dd 0
-loan_emi_dialog db "Estimated Monthly Instalment: RM ", 0
+loan_p dd 0
+loan_r real4 0.0
+loan_n dd 0
+loan_dialog db "Estimated Monthly Instalment: RM ", 0
 
 exit_dialog db "Thank you for using this application", 0
 
+interest_p_dialog db "Principal: RM ", 0
+interest_r_dialog db "Interest rate (in %): ", 0
+interest_n_dialog db "Compounding frequency per year: ", 0
+interest_t_dialog db "Time in years: ", 0
+interest_p dd 0
+interest_r real4 0.0
+interest_n real4 0.0
+interest_t real4 0.0
+interest_dialog db "Final amount: RM ", 0
+
 ;HOCHEEHIN
-debt_dialog db "Compute Debt-to-Income Ratio", 0
 debt_total_dialog db "Total monthly debt payment: RM ", 0
 income_dialog db "Gross monthly income: RM ", 0
 dti_result_dialog db "Debt-to-Income Ratio: ", 0
@@ -182,7 +193,7 @@ menu_loop:
     jae menu
     mov eax, [options + eax * 4]
     mov selected_option, eax
-jump_options:
+option_selected:
     call clear
     mov edx, selected_option
     call writestring
@@ -198,7 +209,7 @@ jump_options:
     cmp edx, offset option_exit
     je main_end
 loan:
-    lea edx, loan_dialog
+    lea edx, values_dialog
     call writestring
     call crlf
 
@@ -206,14 +217,14 @@ loan:
     lea edx, loan_p_dialog
     call writestring
 
-    .if loan_principal == 0
+    .if loan_p == 0
         ; ask for principal
         call readdec
-        mov loan_principal, eax
-        jmp jump_options
+        mov loan_p, eax
+        jmp option_selected
     .endif
 
-    mov eax, loan_principal
+    mov eax, loan_p
     call writedec
     call crlf
 
@@ -221,7 +232,7 @@ loan:
     lea edx, loan_r_dialog
     call writestring
 
-    .if loan_rate == 0
+    .if loan_r == 0
         ; ask for rate
         call read_to_buffer
 
@@ -229,12 +240,12 @@ loan:
         lea edx, buffer
         mov ecx, eax
         call str_to_float
-        jc jump_options
-        mov loan_rate, eax
-        jmp jump_options
+        jc option_selected
+        mov loan_r, eax
+        jmp option_selected
     .endif
 
-    mov eax, loan_rate
+    mov eax, loan_r
     call print_float
     mov al, '%'
     call writechar
@@ -244,23 +255,21 @@ loan:
     lea edx, loan_n_dialog
     call writestring
 
-    .if loan_payment == 0
+    .if loan_n == 0
         ; ask for rate
         call readdec
-        mov loan_payment, eax
-        jmp jump_options
+        mov loan_n, eax
+        jmp option_selected
     .endif
 
-    mov eax, loan_payment
+    mov eax, loan_n
     call writedec
     call crlf
 
-    fld loan_rate
-    fld float_ten
-    fmul float_ten
-    fdiv
+    fld loan_r
+    fdiv float_hundred
     ; calculate (1+r)^n
-    fild loan_payment
+    fild loan_n
     fld1
     fadd st, st(2) ; 1 + r
     ; code from https://www.madwizard.org/programming/snippets?id=36
@@ -281,7 +290,7 @@ loan:
     fld1
     fsub ; (1+r)^n - 1
     fxch st(2)
-    fimul loan_principal ; p * (1+r)^n
+    fimul loan_p ; p * (1+r)^n
     fmul ; p * r * (1+r)^n
     fxch
     fdiv ; p * r * (1+r)^n / ((1+r)^n - 1)
@@ -289,29 +298,140 @@ loan:
 
     ; print EMI
     call crlf
-    lea edx, loan_emi_dialog
+    lea edx, loan_dialog
     call writestring
     mov eax, float_register
     call print_float
     call crlf
 
     ; reset data
-    mov loan_principal, 0
-    mov loan_rate, 0
-    mov loan_payment, 0
+    mov loan_p, 0
+    mov loan_r, 0
+    mov loan_n, 0
 
     jmp wait_input
 interest:
+    lea edx, values_dialog
+    call writestring
+    call crlf
+
+    lea edx, interest_p_dialog
+    call writestring
+
+    .if interest_p == 0
+        call readdec
+        mov interest_p, eax
+        jmp option_selected
+    .endif
+
+    mov eax, interest_p
+    call writedec
+    call crlf
+
+    lea edx, interest_r_dialog
+    call writestring
+
+    .if interest_r == 0
+        call read_to_buffer
+        lea edx, buffer
+        mov ecx, eax
+        call str_to_float
+        jc option_selected
+        mov interest_r, eax
+        jmp option_selected
+    .endif
+
+    mov eax, interest_r
+    call print_float
+    call crlf
+
+    lea edx, interest_n_dialog
+    call writestring
+
+    .if interest_n == 0
+        call read_to_buffer
+        lea edx, buffer
+        mov ecx, eax
+        call str_to_float
+        jc option_selected
+        mov interest_n, eax
+        jmp option_selected
+    .endif
+
+    mov eax, interest_n
+    call print_float
+    call crlf
+
+    lea edx, interest_t_dialog
+    call writestring
+
+    .if interest_t == 0
+        call read_to_buffer
+        lea edx, buffer
+        mov ecx, eax
+        call str_to_float
+        jc option_selected
+        mov interest_t, eax
+        jmp option_selected
+    .endif
+
+    mov eax, interest_t
+    call print_float
+    call crlf
+    call crlf
+
+    fld interest_n
+    fmul interest_t
+    fld interest_r
+    fdiv float_hundred
+    fdiv interest_n
+
+    fst float_register
+    mov eax, float_register
+    call writebin
+    call crlf
+    call print_float
+    call crlf
+
+    fld1
+    fadd
+    fyl2x
+    fld1
+    fld st(1)
+    fprem
+    f2xm1
+    fadd
+    fscale
+    fxch
+    fstp st
+    fimul interest_p
+    fstp float_register
+
+    lea edx, interest_dialog
+    call writestring
+    mov eax, float_register
+    call print_float
+    call crlf
+
+    mov interest_p, 0
+    mov interest_r, 0
+    mov interest_n, 0
+    mov interest_t, 0
+    jmp wait_input
 
 ;HOCHEEHIN
 debt:
+    lea edx, values_dialog
+    call writestring
+    call crlf
+
     lea edx, debt_total_dialog
     call writestring
 
     .if debt_total == 0
         call readdec
         mov debt_total, eax
-        jmp jump_options
+        jmp option_selected
     .endif
 
     mov eax, debt_total
@@ -324,7 +444,7 @@ debt:
     .if income_gross == 0
         call readdec
         mov income_gross, eax
-        jmp jump_options
+        jmp option_selected
     .endif
 
     mov eax, income_gross
@@ -333,8 +453,7 @@ debt:
 
     fld debt_total
     fdiv income_gross
-    fmul float_ten
-    fmul float_ten
+    fmul float_hundred
     fst float_register
 
     call crlf
@@ -555,11 +674,16 @@ print_float proc
     ; float is decimal
     neg cl
     add cl, 23
+    cmp cl, 32
+    jae float_integer_zero
     shr eax, cl
     jmp print_float_integer
 float_is_integer:
     sub cl, 23
     shl eax, cl
+    jmp print_float_integer
+float_integer_zero:
+    mov eax, 0
 print_float_integer:
     call writedec
     mov al, '.'
@@ -581,6 +705,8 @@ print_float_integer:
     ; extract first 8 bits of mantissa
     neg cl
     add cl, 15
+    cmp cl, 24
+    jae print_float_exponent
     mov eax, float_register
     and eax, 007FFFFFh
     or eax, 00800000h
